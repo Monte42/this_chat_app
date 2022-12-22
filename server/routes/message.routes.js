e3aabf4bd0b2149e21e857692203fd2fa6eb9c92
Monte DuMond <gdumond42@gmail.com>
2022-12-21 21:14:35 -0500

    change origin

--- server/routes/message.routes.js
+++ server/routes/message.routes.js
@@ -2,6 +2,6 @@ const MessageController = require("../controllers/message.controller")
 const {authenticate} = require("../config/jwt.config")
 
 module.exports = app => {
-    app.post("/api/chat/newMessage", authenticate, MessageController.createMessage)
-    app.get("/api/chat/:room", authenticate, MessageController.fetchMessageByRoom)
+    app.post("/api/chat/newMessage", MessageController.createMessage)
+    app.get("/api/chat/:room", MessageController.fetchMessageByRoom)
 }
--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(cookieParser())
 
 app.use(cors({
     credentials:true,
-    origin: "*",
+    origin: ["http://localhost:3000","http://10.0.0.47:3000","http://54.146.220.50.3000"],
 }))
 
 require("./config/mongoose.config")
@@ -23,7 +23,7 @@ const server = app.listen(8000, () => console.log("Running..."))
 
 const io = socket(server, {
     cors: {
-        origin: "*",
+        origin: ["http://localhost:3000","http://10.0.0.47:3000","http://54.146.220.50.3000"],
         methods: ["GET","POST","PUT","DELETE"],
         allowedHeaders: ["*"],
         credentials: true
